5616048bd5e115e77989e94d8a148e869daf427d
Rajdeep Shrivastava <rajdeep1999.srivastav@gmail.com>
2023-01-09 16:41:49 +0530

    first project done

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,25 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = [
+  "green",
+  "red",
+  "blue",
+  "purple",
+  "pink",
+  "white",
+  "grey",
+  "cyan",
+];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function () {
+  //get random number between 0 and 3 since size of array is 4
+  const randomNum = getRandomNum();
+
+  document.body.style.backgroundColor = colors[randomNum];
+  color.textContent = colors[randomNum];
+});
+
+function getRandomNum() {
+  return Math.floor(Math.random() * colors.length);
+}
--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,13 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const color = document.querySelector(".color");
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", function () {
+  let hexCol = "#";
+  for (let i = 0; i < 6; i++) {
+    hexCol += hex[Math.floor(Math.random() * hex.length)];
+  }
+  color.textContent = hexCol;
+  document.body.style.backgroundColor = hexCol;
+});
