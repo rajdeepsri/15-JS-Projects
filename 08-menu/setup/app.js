5af07f081553fed335597a4deaffda72a5f3340b
Rajdeep Shrivastava <rajdeep1999.srivastav@gmail.com>
2023-01-12 15:35:00 +0530

    9 - video proj DONE

--- 08-menu/setup/app.js
+++ 08-menu/setup/app.js
@@ -5,7 +5,7 @@ const menu = [
     category: "breakfast",
     price: 15.99,
     img: "./images/item-1.jpeg",
-    desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+    desc: `Fluffy, golden-brown buttermilk pancakes served with butter and maple syrup.`,
   },
   {
     id: 2,
@@ -13,7 +13,7 @@ const menu = [
     category: "lunch",
     price: 13.99,
     img: "./images/item-2.jpeg",
-    desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
+    desc: `Diner Double: Two all-beef patties, special sauce, lettuce, cheese, pickles, onions on a sesame seed bun.`,
   },
   {
     id: 3,
@@ -21,7 +21,7 @@ const menu = [
     category: "shakes",
     price: 6.99,
     img: "./images/item-3.jpeg",
-    desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
+    desc: `A monstrously delicious blend of vanilla ice cream, caramel, and chocolate sauce, topped with a chocolate Godzilla figurine.`,
   },
   {
     id: 4,
@@ -29,7 +29,7 @@ const menu = [
     category: "breakfast",
     price: 20.99,
     img: "./images/item-4.jpeg",
-    desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
+    desc: `a hearty blend of traditional farm-fresh ingredients, perfect for satisfying your cravings for a taste of the countryside.`,
   },
   {
     id: 5,
@@ -37,7 +37,7 @@ const menu = [
     category: "lunch",
     price: 22.99,
     img: "./images/item-5.jpeg",
-    desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
+    desc: `A delicious combination of scrambled eggs with sautéed vegetables and bacon.`,
   },
   {
     id: 6,
@@ -45,7 +45,7 @@ const menu = [
     category: "shakes",
     price: 18.99,
     img: "./images/item-6.jpeg",
-    desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
+    desc: `Rich and creamy Oreo ice cream with chunks of Oreo cookies.`,
   },
   {
     id: 7,
@@ -53,7 +53,7 @@ const menu = [
     category: "breakfast",
     price: 8.99,
     img: "./images/item-7.jpeg",
-    desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
+    desc: `A hearty breakfast dish with crispy bacon, scrambled eggs, and hash browns.`,
   },
   {
     id: 8,
@@ -61,7 +61,7 @@ const menu = [
     category: "lunch",
     price: 12.99,
     img: "./images/item-8.jpeg",
-    desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
+    desc: `A cheeseburger with all the fixings and a side of fries.`,
   },
   {
     id: 9,
@@ -69,45 +69,38 @@ const menu = [
     category: "shakes",
     price: 16.99,
     img: "./images/item-9.jpeg",
-    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+    desc: `A comforting bowl of homemade mac and cheese.`,
+  },
+  {
+    id: 10,
+    title: "steak dinner",
+    category: "dinner",
+    price: 36.99,
+    img: "./images/item-10.jpeg",
+    desc: ` A succulent 8 oz. steak cooked to perfection with a side of roasted vegetables.`,
   },
 ];
 
 const secCenter = document.querySelector(".section-center");
-const filterBtns = document.querySelectorAll(".filter-btn");
+const btnContainer = document.querySelector(".btn-container");
 
 //loads items
 window.addEventListener("DOMContentLoaded", () => {
   displayMenuItems(menu);
+  displayMenuButtons();
 });
 
-//filter items
-filterBtns.forEach((btn) => {
-  btn.addEventListener("click", (e) => {
-    const category = e.currentTarget.dataset.id;
-    const menuCategory = menu.filter(function (menuItem) {
-      if (menuItem.category === category) {
-        return menuItem;
-      }
-    });
-    if (category === "all") {
-      displayMenuItems(menu);
-    } else {
-      displayMenuItems(menuCategory);
-    }
-  });
-});
 const displayMenuItems = (menuItems) => {
   let displayMenu = menuItems.map((item) => {
     return `<article class="menu-item">
-          <img src=${item.img} alt=${item.title} class="photo" />
-          <div class="item-info">
-            <header>
-              <h4>${item.title}</h4>
-              <h4 class="price">$${item.price}</h4>
-            </header>
-            <p class="item-text">
-              ${item.desc}
+    <img src=${item.img} alt=${item.title} class="photo" />
+    <div class="item-info">
+    <header>
+    <h4>${item.title}</h4>
+    <h4 class="price">$${item.price}</h4>
+    </header>
+    <p class="item-text">
+    ${item.desc}
             </p>
           </div>
         </article>`;
@@ -116,4 +109,41 @@ const displayMenuItems = (menuItems) => {
   //now we will join the displayMenu array
   displayMenu = displayMenu.join(""); //"" because else commas will be there when joining
   secCenter.innerHTML = displayMenu;
+};
+
+const displayMenuButtons = () => {
+  //get unique buttons from array so to populate them dynamically
+  const categories = menu.reduce(
+    (values, item) => {
+      if (!values.includes(item.category)) {
+        values.push(item.category);
+      }
+      return values;
+    },
+    ["all"]
+  );
+
+  const categoryBtns = categories
+    .map((category) => {
+      return `<button class="filter-btn" type="button" data-id="${category}">${category}</button>`;
+    })
+    .join("");
+  btnContainer.innerHTML = categoryBtns;
+  const filterBtns = btnContainer.querySelectorAll(".filter-btn");
+  //filter items
+  filterBtns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      const category = e.currentTarget.dataset.id;
+      const menuCategory = menu.filter(function (menuItem) {
+        if (menuItem.category === category) {
+          return menuItem;
+        }
+      });
+      if (category === "all") {
+        displayMenuItems(menu);
+      } else {
+        displayMenuItems(menuCategory);
+      }
+    });
+  });
 };
